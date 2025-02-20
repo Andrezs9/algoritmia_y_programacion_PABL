04ec981dda18beda8f07e7cd1bbbe2f317096746
Andrezs9 <andreslopsz99@gmail.com>
2025-02-19 23:35:05 -0500

    Add files via upload

---
+++ Taller estructuras de control ejercicios/ejercicios Python/ejercicio 9 salario neto.py
@@ -0,0 +1,11 @@
+#Entrada
+horas_trabajadas = float(input("Ingrese la cantidad de horas trabajas: "))
+valor_por_hora = float(input("Ingrese el valor por hora trabajada: "))
+
+#caja negra
+salario_bruto = horas_trabajadas * valor_por_hora
+descuento_por_impuesto = salario_bruto * 0.20
+salario_neto = salario_bruto - descuento_por_impuesto
+
+#salida
+print("Su salario neto es de: ",salario_neto)
